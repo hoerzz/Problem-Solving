079943bf7cb194dc58bd256a3bc4c3ff99d120c5
hoerzz <rudinhoer160@gmail.com>
2021-10-11 03:56:51 -0700

    Problem-3

--- Assets/Script/BallControl.cs
+++ Assets/Script/BallControl.cs
@@ -7,28 +7,36 @@ public class BallControl : MonoBehaviour
     private Rigidbody2D rigidbody2D;
     public float xForce;
     public float yForce;
-    public float speed;
+    public float BallSpeed;
+    Vector3 lastVelocity;
 
-    // Start is called before the first frame update
     void Start()
     {
         rigidbody2D = GetComponent<Rigidbody2D>();
     }
+    void Update()
+    {
+        PushBall();
+        lastVelocity = rigidbody2D.velocity;
+    }
+    //Bola bergerak random dengan kecepatan konstan
     void PushBall()
     {
         float arahRandom = Random.Range(0, 2);
         if (arahRandom < 1.0f)
         {
-            rigidbody2D.AddForce(new Vector2(-xForce, yForce).normalized * speed);
+            rigidbody2D.AddForce(new Vector2(-xForce, yForce).normalized * BallSpeed);
         }
         else
         {
-            rigidbody2D.AddForce(new Vector2(xForce, yForce).normalized * speed);
+            rigidbody2D.AddForce(new Vector2(xForce, yForce).normalized * BallSpeed);
         }
     }
-    // Update is called once per frame
-    void Update()
-    {
-        PushBall();
+    //Untuk Memantulkan bola
+    private void OnCollisionEnter2D(Collision2D other) {
+        var speed = lastVelocity.magnitude;
+        var dir = Vector2.Reflect(lastVelocity.normalized, other.contacts[0].normal);
+
+        rigidbody2D.velocity = dir * Mathf.Max(speed, BallSpeed);
     }
 }
