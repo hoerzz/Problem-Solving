e06c2a2a3f6b3bff0103afa1215746730049e21c
hoerzz <rudinhoer160@gmail.com>
2021-10-11 23:20:20 -0700

    Problem-8

---
+++ Assets/Script/SpriteSpawnerWaitForSeconds.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpriteSpawnerWaitForSeconds : SpriteSpawner
+{
+    public IEnumerator SpawnSprite()
+    {
+        if (transform.childCount < 10)
+        {
+            float randomX = Random.Range(-6, 6);
+            float randomY = Random.Range(-4, 4);
+
+            Instantiate(kotak, new Vector2(randomX, randomY), Quaternion.identity); 
+            
+        }
+        yield return new WaitForSeconds(3);
+        StartCoroutine(SpawnSprite()); 
+    }
+}
