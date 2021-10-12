eda418914e7838c13df22752d3c07ec67e292ebe
hoerzz <rudinhoer160@gmail.com>
2021-10-11 20:36:09 -0700

    Problem-6

---
+++ Assets/Script/SpriteSpawner.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpriteSpawner : MonoBehaviour
+{
+    public GameObject Sprite;
+    public float xMin, xMax, yMin, yMax;
+   
+    // Start is called before the first frame update
+    void Start()
+    {
+        Spawn();
+    }
+    
+    void Spawn()
+    {
+        int numberToSpawn = Random.Range(5, 10);
+        for (int i = 0; i < numberToSpawn; i++)
+        {
+            float randomX = Random.Range(xMin, xMax);
+            float randomY = Random.Range(yMin, yMax);
+            Instantiate(Sprite, new Vector2(randomX, randomY), Quaternion.identity);
+        }
+
+    }
+}
