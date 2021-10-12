f1f72b22a9388189c6d24abef2782ea69272a48b
hoerzz <rudinhoer160@gmail.com>
2021-10-11 22:21:55 -0700

    Problem-7

--- Assets/Script/FollowMouse.cs
+++ Assets/Script/FollowMouse.cs
@@ -6,6 +6,7 @@ public class FollowMouse : MonoBehaviour
 {
     private Vector3 mousePosition;
     public float moveSpeed = 0.1f;
+    public ScoreManager score;
     // Start is called before the first frame update
     void Start()
     {
@@ -18,5 +19,14 @@ public class FollowMouse : MonoBehaviour
         mousePosition = Input.mousePosition;
         mousePosition = Camera.main.ScreenToWorldPoint(mousePosition);
         transform.position = Vector2.Lerp(transform.position, mousePosition, moveSpeed);
+    }
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.gameObject.tag == "Obstacle")
+        {
+            score.scoreadd();
+            Destroy(col.gameObject);
+        }
     }
 }
