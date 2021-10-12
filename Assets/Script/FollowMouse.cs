66e9076064d04a609816ecddff5f716f6cb67753
hoerzz <rudinhoer160@gmail.com>
2021-10-12 04:07:58 -0700

    Problem-9

--- Assets/Script/FollowMouse.cs
+++ Assets/Script/FollowMouse.cs
@@ -7,6 +7,7 @@ public class FollowMouse : MonoBehaviour
     private Vector3 mousePosition;
     public float moveSpeed = 0.1f;
     public ScoreManager score;
+    public SpawnRedSquere redSquere;
     // Start is called before the first frame update
     void Start()
     {
@@ -25,8 +26,19 @@ public class FollowMouse : MonoBehaviour
     {
         if (col.gameObject.tag == "Obstacle")
         {
+            transform.localScale += new Vector3(0.5f,0.5f,0.5f);
             score.scoreadd();
             Destroy(col.gameObject);
+        }
+    }
+    private void OnTriggerEnter2D(Collider2D other) 
+    {
+        if (other.gameObject.tag == "RedSquere")
+        {
+            transform.localScale += new Vector3(-0.5f,-0.5f,-0.5f);
+            score.scoremin();
+            redSquere.RedSquereSpawner();
+            Destroy(other.gameObject);
         }
     }
 }
